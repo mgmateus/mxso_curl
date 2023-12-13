a7e3afc1cdb64da4362c03c0539a7666b60a8fe8
mgmateus <eng.mgmateus@gmail.com>
2023-12-13 12:20:00 -0300

    clouds and images

--- mxso_curl/scripts/image_collect.py
+++ mxso_curl/scripts/image_collect.py
@@ -21,21 +21,22 @@ if __name__ == "__main__":
     
     targetp = [(30, (np.pi/3)), (30, (5*np.pi/6)), (30, (5*np.pi/4)), (30, (7*np.pi/4))]
     
+
     env.client.takeoffAsync(vehicle_name='Hydrone').join()
     env.client.hoverAsync("Hydrone").join()
+    z = 100
 
+    env.client.moveToZAsync(z, 1, vehicle_name='Hydrone').join()
     
 
     path = [env.spawn_resources._oriented_target_vision_to_vehicle('Hydrone', 'eolic', tp[0], 20, tp[1]) for tp in targetp]
     rospy.logwarn(f"path -> {path}")
     env.client.simSetTraceLine([1.0, 0.0, 0.0, 1.0], 50, "Hydrone")
-    z = 20
+    """
+    
     for x, y, phi in path:
         rospy.logwarn(f"pose -> {x, y, phi}")
-        env.client.moveToPositionAsync(x, y, z, 10, drivetrain=DrivetrainType.ForwardOnly)
-        pos = env.client.getMultirotorState().kinematics_estimated.position
-        while abs(pos.x_val -x) >= 0.05 and abs(pos.x_val - y) >= 0.05:
-            pos = env.client.getMultirotorState().kinematics_estimated.position
+        env.client.moveToPositionAsync(x, y, z, 10, drivetrain=DrivetrainType.MaxDegreeOfFreedom).join()
         time.sleep(2)
         env.spawn_resources._set_vehicle_pose('Hydrone', (x, y, z), (0, 0, phi))
         time.sleep(5)
@@ -43,4 +44,4 @@ if __name__ == "__main__":
         time.sleep(2)
         rospy.logwarn(f"finishing pose... -> {x, y, phi}")
     
-    
+    """
